9c3d104b8352a1e79cb1b4d9be62ae1aadefedbb
Murtaza1195 <108726538+Murtaza1195@users.noreply.github.com>
2022-07-05 17:46:30 +0500

    newfile

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import logo from '../Component/images/nexus.png';
+
 function Header() {
     return (
         <nav>
-            <img src="../Component/images/nexus.png" alt=""/>
+            <img src={logo} alt="Logo" />;
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">ToDoList</a></li>
